2bf74b02e1a45bb727c58b4df737f23daf029af9
Jeffrey Seneff <jeffsnff@gmail.com>
2024-01-28 16:04:35 -0700

    Put everything into one object

--- part1/Excercises/exercisePart1/src/App.jsx
+++ part1/Excercises/exercisePart1/src/App.jsx
@@ -3,18 +3,21 @@ import Total from "./Total.jsx";
 import Header from "./Header.jsx";
 
 const App = () => {
+  const course = {
+    courseName : 'Half stack application development',
+    sections : [
+      {sectionName : 'Fundamentals of React', numExercises : 10},
+      {sectionName : 'Using props to pass data', numExercises: 7},
+      {sectionName: 'State of a component', numExercises: 14}
+    ]
+    
+  }
 
-  const course = 'Half stack application development'; // Header
-  const parts = [
-    {name: 'Fundamentals of React', exercises: 10},
-    {name: 'Using props to pass data', exercises: 7},
-    {name: 'State of a component', exercises: 14}
-  ]
   return(
     <>
       <Header course={course} />
-      <Content parts={parts}/>
-      <Total numberExercises={parts}/>
+      {/* <Content parts={parts}/>
+      <Total numberExercises={parts}/> */}
     </>
   )
 }
