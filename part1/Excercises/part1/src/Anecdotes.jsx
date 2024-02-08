c7dc8b5deb23b4cab2e3225345875106efc5105f
Jeffrey Seneff <jeffsnff@gmail.com>
2024-02-08 07:39:38 -0700

    Finished 1.14

--- part1/Excercises/part1/src/Anecdotes.jsx
+++ part1/Excercises/part1/src/Anecdotes.jsx
@@ -18,6 +18,7 @@ const Anecdotes = () => {
   const [ vote, setVote ] = useState(
     {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0})
   const [ displayVote, setDisplayVote ] = useState(selected)
+  const [ displayHighestAnecdote, setDisplayHighestAnecdote ] = useState()
   
   const handleAnecdote = () => {
     const number = Math.floor(Math.random() * 8);
@@ -28,14 +29,30 @@ const Anecdotes = () => {
     let copyVote = {...vote};
     copyVote[selected] = copyVote[selected] + 1;
     setVote(copyVote);
+    findHighestRating(copyVote)
   }
-  
+
+  const findHighestRating = (object) => {
+    const values = Object.values(object);
+    // const maxRating = Math.max.apply(Math, values); This was the first one I was able to implement, but I don't FULLY understand it. 
+    const maxRating = Math.max(...values); // This one I understand more so I am going with this one.
+    
+    for(const key in object){
+      if(object[key] === maxRating){
+        setDisplayHighestAnecdote(anecdotes[key])
+      }
+    }
+  }
+
   return (
     <>
+      <h1>Anecdote of the Day</h1>
       <p>Qoute : {anecdotes[selected]}</p>
       <p>Votes : {displayVote}</p>
       <Button onClickFunction={handleVote} buttonName={'Vote'} />
       <Button onClickFunction={handleAnecdote} buttonName={'New Anecdote'} />
+      <h1>Highest Rated Anecdote</h1>
+      <p>{displayHighestAnecdote}</p>
     </>
   );
 }
