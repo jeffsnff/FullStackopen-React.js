5a1c1de23515afa9e625aec429c3474a135163ad
Jeffrey Seneff <jeffsnff@gmail.com>
2024-02-12 19:36:39 -0700

    implemented a form while learning about forms

--- Part3/Learning/src/App.jsx
+++ Part3/Learning/src/App.jsx
@@ -3,7 +3,22 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [ newNote, setNewNote ] = useState('Add a note')
 
+  const addNote =(event) => {
+    event.preventDefault();
+    const newEntry = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+    setNotes(notes.concat(newEntry));
+    setNewNote('');
+  }
+
+  const noteHandleChange = (event) => {
+    setNewNote(event.target.value);
+  }
   return (
     <div>
       <h1>Notes</h1>
@@ -12,6 +27,12 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote} >
+        <input
+          value={newNote}
+          onChange={noteHandleChange} />
+        <button type='submit'>Save</button>
+      </form>
     </div>
   )
 }
