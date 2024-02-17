0c1b27b4902f4e69f03336c6cea578f7ee02d89c
Jeffrey Seneff <jeffsnff@gmail.com>
2024-02-17 16:12:50 -0700

    Completed 2.6

--- Part3/Exercises/src/App.jsx
+++ Part3/Exercises/src/App.jsx
@@ -1,8 +1,43 @@
+import { useState } from 'react';
+
 const App = () => {
   
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ]);
+  const [newName, setNewName] = useState('')
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newEntry = {
+      name: newName
+    }
+    setPersons(persons.concat(newEntry));
+    setNewName('');
+  }
+
+  const personList = persons.map((person) => {
+    return <div key={person.name} >{person.name}</div>
+  })
   return(
     <>
-      <h1>Part 3 Exercises</h1>
+      <h1>Phonebook</h1>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">Add Name</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {personList}
+      </div>
     </>
   );
 }
