738249efb808d17b058a096da566f3911aa20c60
Jeffrey Seneff <jeffsnff@gmail.com>
2024-02-18 16:55:07 -0700

    Completed 2.9

--- Part3/Exercises/src/App.jsx
+++ Part3/Exercises/src/App.jsx
@@ -3,10 +3,14 @@ import { useState } from 'react';
 const App = () => {
   
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '3215763465', id: 1}
+    {name: 'Arto Hellas', number: '3215763465', id: 1},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filteredName, setFilterName] = useState('');
 
   const nameHandleChange = (event) => {
     setNewName(event.target.value);
@@ -14,15 +18,20 @@ const App = () => {
   const numberHandleChange = (event) => {
     setNewNumber(event.target.value);
   }
+  const searchHandleChange = (event) => {
+    setFilterName(event.target.value.toLowerCase());
+  }
 
   const addPerson = (event) => {
     event.preventDefault();
+    // Creates new entry object
     const newEntry = {
       name: newName,
       number: newNumber,
       id: persons.length + 1
     }
 
+    // Looks to see if person is already in the phonebook
     if(persons.find((person) => person.name === newEntry.name)){
       alert(`${newEntry.name} already exist in the Phonebook.`);
       setNewName('');
@@ -35,13 +44,22 @@ const App = () => {
     }
   }
 
-  const personList = persons.map((person) => {
+  // This filters the persons array
+  const filteredList = persons.filter( person => person.name.toLocaleLowerCase().includes(filteredName));
+
+  // Takes filtered list and maps over it
+  const personList = filteredList.map((person) => {
     return <div key={person.id} >{person.name} : {person.number}</div>
   })
+  
   return(
     <>
       <h1>Phonebook</h1>
+      <div>
+        Search: <input value={filteredName} onChange={searchHandleChange} /> 
+      </div>
       <form onSubmit={addPerson}>
+        <h2>Add new contact</h2>
         <div>
           name: <input value={newName} onChange={nameHandleChange} />
         </div>
