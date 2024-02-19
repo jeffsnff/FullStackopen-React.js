eab3dcdd2e752691342183a09f7290b422ab72d3
Jeffrey Seneff <jeffsnff@gmail.com>
2024-02-19 06:53:38 -0700

    updated 2.10 with a new Person file

--- Part3/Exercises/src/Contact.jsx
+++ Part3/Exercises/src/Contact.jsx
@@ -1,10 +1,13 @@
 import Header from './Header.jsx';
+import Person from './Person.jsx';
 
 function Contact({ contactList }){
   
   // Takes filtered list and maps over it
   const personList = contactList.map((person) => {
-    return <li key={person.id} >{person.name} : {person.number}</li>
+    return (
+      <Person key={person.id} name={person.name} number={person.number} />
+    )
   })
 
   return(
