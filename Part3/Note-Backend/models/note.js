b8fae431e9130281227fb6bcea9f465dc8de87bb
Jeffrey Seneff <jeffsnff@gmail.com>
2024-05-24 07:00:23 -0600

    removed env require cleaned up some syntax

--- Part3/Note-Backend/models/note.js
+++ Part3/Note-Backend/models/note.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
-
 const url = process.env.MONGODB_URI;
+
 mongoose.set('strictQuery', false);
 mongoose.connect(url).then( result => {
-  console.log('Connected to MongoDB')
+  console.log('Connected to MongoDB');
 }).catch(error => {
-  console.log('Error conntecting to MongoDB:', error.message)
+  console.log('Error conntecting to MongoDB:', error.message);
 })
 
 const noteSchema = new mongoose.Schema({
@@ -16,10 +15,10 @@ const noteSchema = new mongoose.Schema({
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
 module.exports = mongoose.model('Note', noteSchema);
