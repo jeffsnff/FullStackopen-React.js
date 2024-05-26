4e6fbc7dc45b2d32aff7a5d09d7cedc9ea831d0f
Jeffrey Seneff <jeffsnff@gmail.com>
2024-05-26 14:44:32 -0600

    Implimented request logger, unknowen endpoint, and reformatted code

--- Part3/Note-Backend/index.js
+++ Part3/Note-Backend/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT;
 const Note = require('./models/note.js');
 const app = express();
+const PORT = process.env.PORT;
+
 app.use(cors());
 app.use(express.static('dist'))
 app.use(express.json());
@@ -30,6 +31,28 @@ let notes = [
     important: true
   }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if(error.name === 'CastError'){
+    response.status(400).send({ error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
 
 // This is the first page that loads. This does NOT get all notes
 app.get('/', (request, response) => {
@@ -98,13 +121,9 @@ app.delete('/api/notes/:id', (request, response) => {
     .catch(error => next(error));
 })
 
-const errorHandler = (error, request, response, next) => {
-  // console.log(error)
-  if(error.name === 'CastError'){
-    response.status(400).send({ error: 'malformatted id'})
-  }
-  next(error)
-}
+
 app.use(errorHandler)
+app.use(unknownEndpoint)
+
 app.listen(PORT);
 console.log(`Sever is running on PORT : ${PORT}`);
