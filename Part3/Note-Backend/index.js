c65493002fca7be98e40d1a0abebb1b089aa5268
Jeffrey Seneff <jeffsnff@gmail.com>
2024-05-26 14:36:55 -0600

    Implimented Put request for notebackend

--- Part3/Note-Backend/index.js
+++ Part3/Note-Backend/index.js
@@ -75,6 +75,21 @@ app.post('/api/notes', (request, response) => {
 
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updateNote => {
+      response.json(updateNote)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/notes/:id', (request, response) => {
   Note.findByIdAndDelete(request.params.id)
     .then(result => {
