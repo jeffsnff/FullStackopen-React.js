3dd89ba8933d53197c9726260c50ba1c615ec15f
Jeffrey Seneff <jeffsnff@gmail.com>
2024-05-27 16:47:30 -0600

    Updated error handling for POST request

--- Part3/Note-Backend/index.js
+++ Part3/Note-Backend/index.js
@@ -21,6 +21,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if(error.name === 'CastError'){
     response.status(400).send({ error: 'malformatted id'})
+  }else if(error.name === 'ValidationError'){
+    return response.status(400).json({error : error.message })
   }
   next(error)
 }
@@ -56,22 +58,19 @@ app.get('/api/notes/:id', (request, response, next) => {
   })
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
-  if(body.content === undefined){
-    return response.status(400).json({
-      error: "Content Missing"
-    });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
+  note.save()
+    .then(savedNote => {
     response.json(savedNote)
   })
+    .catch(error => next(error))
 
 })
 
